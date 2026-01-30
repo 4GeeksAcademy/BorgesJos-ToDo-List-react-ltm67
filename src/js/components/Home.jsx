607c4a6d1bc16c23c0766f05c1745ed5dced21aa
José Borges <110873863+BorgesJos@users.noreply.github.com>
2026-01-30 23:35:34 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+
+
+const Home = () => {
+  const [nuevoTodo, setNuevoTodo] = useState("Tarea Ejemplo");
+  const [taskList, setTaskList] = useState([]);
+
+  const handleChange = (e) => {
+    setNuevoTodo(e.target.value);
+  }
+
+  const deleteTask = (index) => {
+    const newList = taskList.filter((todo, i) => i !== index);
+    setTaskList(newList)
+  }
+  return (
+    <div className="text-center bg-light">
+      <h1 className="text-center mt-5">To Do List</h1>
+      <div className="d-flex justify-content-center">
+        <input type="text" className="form-control w-75 mb-3"
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              setTaskList([...taskList, nuevoTodo])
+              setNuevoTodo("")
+              e.target.value = "";
+            }
+          }}
+          onChange={handleChange} />
+      </div>
+      {taskList.map((task, index) =>
+        <div className="d-flex justify-content-center p-2" key={index}>
+          <p className="fs-1 mx-2">{task}</p>
+          <button className="btn btn-outline-danger float-end" onClick={() => deleteTask(index)}>x</button>
+        </div>)}
+
+    </div>
+  );
 };
 
 export default Home;
